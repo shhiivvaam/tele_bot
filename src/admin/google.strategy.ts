b576db0f170610a1b19180b08b536639049358ff
Shivam Kumar <life.shivam2394@gmail.com>
2024-11-12 11:14:07 +0530

    changing auth url

--- src/admin/google.strategy.ts
+++ src/admin/google.strategy.ts
@@ -8,7 +8,7 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
         super({
             clientID: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-            callbackURL: `${process.env.BACKEND_URL}/auth/google/callback`,
+            callbackURL: `${process.env.BACKEND_URL}/admin/google/callback`,
             scope: ['email', 'profile'],
         });
     }
